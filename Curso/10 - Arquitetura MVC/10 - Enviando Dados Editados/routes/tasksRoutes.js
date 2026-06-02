58a798c37ffad353fae9928fbeab83ccfb96fb28
thiagomartendal <thiagomartendal@gmail.com>
2026-06-01 22:18:15 -0300

    Add files via upload

---
+++ Curso/10 - Arquitetura MVC/10 - Enviando Dados Editados/routes/tasksRoutes.js
@@ -0,0 +1,16 @@
+import { Router } from 'express'
+import TaskController from '../controllers/TaskController.js'
+
+const router = Router()
+
+// Rotas GET
+router.get('/', TaskController.showTasks)
+router.get('/add', TaskController.createTask)
+router.get('/edit/:id', TaskController.updateTask)
+
+// Rotas POST
+router.post('/add', TaskController.createTaskSave)
+router.post('/remove', TaskController.removeTask)
+router.post('/edit', TaskController.updateTaskPost)
+
+export {router as tasksRouter}
